68a7175335808d9d564a192c531a0730ea082abe
Kevin Dierkx <kevin@distortedfusion.com>
2021-11-29 18:21:55 +0100

    Removed various unused files

--- config/app.php
+++ config/app.php
@@ -168,9 +168,7 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
+        // App\Providers\AuthServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
     ],
 
